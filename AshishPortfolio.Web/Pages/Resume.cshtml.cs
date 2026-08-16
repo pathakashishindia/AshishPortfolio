a2e5b990bc89d733538539c67f88f286c1607ffc
Ashish Pathak <pathak.ashishindia@gmail.com>
2026-08-17 01:56:55 +0530

    feat: complete .net portfolio with viewmodels, visual resume and project assets

---
+++ AshishPortfolio.Web/Pages/Resume.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ResumeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
